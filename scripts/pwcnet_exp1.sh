2b0223e7f6f4223bfb9020df0bdf8abe1bf1cf70
Prajnan Goswami <prajnan.videoworks@gmail.com>
2022-08-18 14:36:51 -0400

    added experiment

---
+++ scripts/pwcnet_exp1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --time=120:00:00
+#SBATCH --job-name=pwcnet_exp1
+#SBATCH --partition=jiang
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=../../results/pwcnet/outs/exp1.out
+
+module load cuda/11.3
+cd ../
+python train.py --model "PWCNetV1" \
+                --model_cfg "./configs/pwcnet/models/nnflow_v1.yaml" \
+                --train_cfg "./configs/pwcnet/trainer/chairs_v1.yaml" \
+                --device "0" \
+                --log_dir "../results/pwcnet/logs/exp1" \
+                --ckpt_dir "../results/pwcnet/ckpts/exp1" \
+                --batch_size 8 \
+                --epochs 432 \
+                --train_crop_size 384 448 \
+                --val_crop_size 384 448 
