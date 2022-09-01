c99ec3a048c1910db685a6f037e15aad2267a0a6
Prajnan Goswami <prajnan.videoworks@gmail.com>
2022-09-01 16:05:43 -0400

    add flying things config

--- scripts/pwcnet_exp13_2.sh
+++ scripts/pwcnet_exp13_2.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 #SBATCH --time=168:00:00
-#SBATCH --job-name=pwcnet_exp12_2
+#SBATCH --job-name=pwcnet_exp13_2
 #SBATCH --partition=jiang
 #SBATCH --mem=24G
 #SBATCH --gres=gpu:a5000:1
 #SBATCH --cpus-per-task=8
-#SBATCH --output=../../results/pwcnet/outs/exp12_2.out
+#SBATCH --output=../../results/pwcnet/outs/exp13_2.out
 
-# Same as Experiment 11 but with RAFT training strategy 
+# Resume Experiment 13_1 with Flying Things
 
 module load cuda/11.3
 cd ../
 python train.py --model "PWCNetV2" \
                 --model_cfg "./configs/pwcnet/models/nnflow_v2.yaml" \
-                --train_cfg "./configs/pwcnet/trainer/chairs_v5_2.yaml" \
+                --train_cfg "./configs/pwcnet/trainer/things_v1_1.yaml" \
                 --device "0" \
-                --log_dir "../results/pwcnet/logs/exp12_2" \
-                --ckpt_dir "../results/pwcnet/ckpts/exp12_2" \
+                --log_dir "../results/pwcnet/logs/exp13_2" \
+                --ckpt_dir "../results/pwcnet/ckpts/exp13_2" \
                 --batch_size 6 \
                 --num_steps 100000 \
                 --train_crop_size 400 720 \
                 --val_crop_size 400 720 \
-                --resume_ckpt '../results/pwcnet/ckpts/exp12_1/pwcnetv2_step99000.pth' \
+                --resume_ckpt '../results/pwcnet/ckpts/exp13_1/pwcnetv2_step99000.pth' \
                 --resume_iteration 99900 \
