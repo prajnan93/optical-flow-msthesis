0c8178f99b1f47c71fa17ab8f864a88d489450ea
Prajnan Goswami <prajnan.videoworks@gmail.com>
2022-09-08 21:22:31 -0400

    added scripts

---
+++ scripts_flownetC/pwcnet_exp01.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --time=240:00:00
+#SBATCH --job-name=flownet_c_exp01
+#SBATCH --partition=jiang
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=../../results/flownet_c/outs/exp01.out
+
+# PWCNet training with RAFT Augmentation, Training settings and PWCNet Normalization
+
+module load cuda/11.3
+cd ../
+python train.py --model "PWCNet" \
+                --model_cfg "./configs/flownet_c/models/flownet_c.yaml" \
+                --train_cfg "./configs/flownet_c/trainer/chairs_v1_0.yaml" \
+                --device "0" \
+                --log_dir "../results/flownet_c/logs/exp01" \
+                --ckpt_dir "../results/flownet_c/ckpts/exp01" \
+                --batch_size 10 \
+                --start_iteration 1 \
+                --num_steps 100100 \
+                --train_crop_size 384 448 \
+                --val_crop_size 384 448 
---
+++ scripts_flownetC/pwcnet_exp02.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --time=240:00:00
+#SBATCH --job-name=flownet_c_exp02
+#SBATCH --partition=jiang
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=../../results/flownet_c/outs/exp02.out
+
+# PWCNet training with RAFT Augmentation, Normalization and Training settings
+
+module load cuda/11.3
+cd ../
+python train.py --model "PWCNet" \
+                --model_cfg "./configs/flownet_c/models/flownet_c.yaml" \
+                --train_cfg "./configs/flownet_c/trainer/chairs_v1_1.yaml" \
+                --device "0" \
+                --log_dir "../results/flownet_c/logs/exp02" \
+                --ckpt_dir "../results/flownet_c/ckpts/exp02" \
+                --batch_size 10 \
+                --start_iteration 1 \
+                --num_steps 100100 \
+                --train_crop_size 384 448 \
+                --val_crop_size 384 448 
