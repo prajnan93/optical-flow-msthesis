821ae94b7cf49176d4b0072a891c6946fab69158
Prajnan Goswami <prajnan.videoworks@gmail.com>
2022-09-13 10:38:55 -0400

    added training scripts for flownet c

---
+++ scripts_flownet_c/exp04.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --time=240:00:00
+#SBATCH --job-name=flownet_c_exp04
+#SBATCH --partition=jiang
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=../../results/flownet_c/outs/exp04.out
+
+# FlowNetC training with RAFT Augmentation, Training settings, PWCNet Normalization 
+# FLOW_SCALE_FACTOR=1.0 
+
+module load cuda/11.3
+cd ../
+python train.py --model "FlowNetC" \
+                --model_cfg "./configs/flownet_c/models/flownet_c.yaml" \
+                --train_cfg "./configs/flownet_c/trainer/chairs_v2_1.yaml" \
+                --device "0" \
+                --log_dir "../results/flownet_c/logs/exp04" \
+                --ckpt_dir "../results/flownet_c/ckpts/exp04" \
+                --batch_size 10 \
+                --start_iteration 1 \
+                --num_steps 100100 \
+                --train_crop_size 384 448 \
+                --val_crop_size 384 448 
