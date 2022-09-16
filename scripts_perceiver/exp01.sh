19daa39b382e132fbbe8edd5dac2f81120282b9d
prajnan93 <prajnan.videoworks@gmail.com>
2022-09-16 11:20:13 -0400

    update configs and scripts

--- scripts_perceiver/exp01.sh
+++ scripts_perceiver/exp01.sh
@@ -3,8 +3,8 @@
 #SBATCH --time=240:00:00
 #SBATCH --job-name=perceiver_exp01
 #SBATCH --partition=jiang
-#SBATCH --mem=24G
-#SBATCH --gres=gpu:a5000:1
+#SBATCH --mem=96G
+#SBATCH --gres=gpu:a6000:2
 #SBATCH --cpus-per-task=8
 #SBATCH --output=../../results/perceiver/outs/exp01.out
 
@@ -14,12 +14,13 @@ module load cuda/11.3
 cd ../
 python train.py --model "Perceiver" \
                 --model_cfg "./configs/perceiver/models/perceiver.yaml" \
-                --train_cfg "./configs/perceiver/trainer/chairs_v1_0.yaml" \
-                --device "0" \
+                --train_cfg "./configs/perceiver/trainer/chairs_ddp_v1_0.yaml" \
+                --device "all" \
                 --log_dir "../results/perceiver/logs/exp01" \
                 --ckpt_dir "../results/perceiver/ckpts/exp01" \
-                --batch_size 10 \
+                --batch_size 4 \
                 --start_iteration 1 \
                 --num_steps 100100 \
                 --train_crop_size 368 496 \
-                --val_crop_size 368 496 
+                --val_crop_size 368 496 \
+                --world_size 2
