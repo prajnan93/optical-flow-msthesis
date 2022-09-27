c0da4f980facfad9e4f1b534f9eb088689016a66
Prajnan Goswami <prajnan.videoworks@gmail.com>
2022-09-27 11:09:37 -0400

    restructure dir and add gmflow

--- nnflow/__init__.py
+++ nnflow/__init__.py
@@ -1,8 +1,5 @@
-# from .pwcnet_1 import PWCNetV1, PWCNetV2
-# from .pwcnet_2 import PWCNetV3
-# from .losses import MultiLevelEPE, MultiScale
-# from .trainer import CustomTrainer, CustomDistributedTrainer
+from .models.perceiver_io import Perceiver
+from .models.gmflow import GMFlow
 from .inference import eval_model
 from .flow_viz import flow_to_image
-from .perceiver_io import Perceiver
 from .kubric import CustomDataloaderCreator
