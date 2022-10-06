763441cf8137627b573bf16f3ed12d10b8f33284
prajnan93 <prajnan.videoworks@gmail.com>
2022-10-06 09:10:53 -0400

    add raft kubric training without augmentation

--- nnflow/models/gmflow/__init__.py
+++ nnflow/models/gmflow/__init__.py
@@ -1,1 +1,8 @@
 from .gmflow import GMFlow, GMFlow_OG
+from .backbone import CNNEncoder
+from .transformer import FeatureTransformer, FeatureFlowAttention
+from .matching import global_correlation_softmax, local_correlation_softmax
+from .geometry import flow_warp
+from .utils import normalize_img, feature_add_position
+from .swin_v1 import SwinEncoderV1
+from .swin_v2 import SwinEncoderV2
