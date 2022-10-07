867ce81f1dd4cb6cbc6143d123e598d1dba75e16
prajnan93 <prajnan.videoworks@gmail.com>
2022-10-07 12:01:30 -0400

    Kubric dataset validation split conversion

--- eval.sh
+++ eval.sh
@@ -3,11 +3,11 @@
 
 module load cuda/11.3
 
-python eval.py --model "GMFlow" \
-                --model_cfg "./configs/gmflow/models/gmflow_v01.yaml" \
-                --model_weights_path "../results/gmflow/ckpts/exp04/gmflow_best.pth" \
-                --dataset 'things_clean' \
+python eval.py --model "GMFlowV2" \
+                --model_cfg "./configs/gmflow/models/gmflow_v02.yaml" \
+                --model_weights_path "../results/gmflow/ckpts/exp06/gmflowv2_step100000.pth" \
+                --dataset 'flyingchairs' \
                 --batch_size 1 \
                 --mean 0.485 0.456 0.406 \
                 --std 0.229 0.224 0.225 \
-                --pad_divisor 16
+                --pad_divisor 1
