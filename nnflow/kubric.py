c4d795437df92018f5409bf94b05264bc082e1c7
prajnan93 <prajnan.videoworks@gmail.com>
2022-10-08 19:52:07 -0400

    update training configs

--- nnflow/kubric.py
+++ nnflow/kubric.py
@@ -196,6 +196,98 @@ class Kubric(data.Dataset):
         """
         return self.sample_count
 
+class KubricV2(BaseDataset):
+    """
+    Dataset Class for preparing the MPI Sintel Synthetic dataset for training and validation.
+
+    Parameters
+    ----------
+    root_dir : str
+        path of the root directory for the MPI Sintel datasets
+    split : str, default : "training"
+        specify the training or validation split
+    dstype : str, default : "frames_cleanpass"
+        specify dataset type
+    is_prediction : bool, default : False
+        If True, only image data are loaded for prediction otherwise both images and flow data are loaded
+    init_seed : bool, default : False
+        If True, sets random seed to worker
+    append_valid_mask : bool, default :  False
+        If True, appends the valid flow mask to the original flow mask at dim=0
+    crop: bool, default : True
+        Whether to perform cropping
+    crop_size : :obj:`tuple` of :obj:`int`
+        The size of the image crop
+    crop_type : :obj:`str`, default : 'center'
+        The type of croppping to be performed, one of "center", "random"
+    augment : bool, default : True
+        If True, applies data augmentation
+    aug_params : :obj:`dict`, optional
+        The parameters for data augmentation
+    norm_params : :obj:`dict`, optional
+        The parameters for normalization
+    """
+
+    def __init__(
+        self,
+        root_dir,
+        split="training",
+        use_backward_flow=False,
+        is_prediction=False,
+        init_seed=False,
+        append_valid_mask=False,
+        crop=False,
+        crop_size=(256, 256),
+        crop_type="center",
+        augment=True,
+        aug_params={
+            "color_aug_params": {"aug_prob": 0.2},
+            "eraser_aug_params": {"aug_prob": 0.5},
+            "spatial_aug_params": {"aug_prob": 0.8},
+            "translate_params": {"aug_prob": 0.8},
+            "rotate_params": {"aug_prob": 0.8},
+        },
+        norm_params={"use": False},
+    ):
+        super(KubricV2, self).__init__(
+            init_seed=init_seed,
+            is_prediction=is_prediction,
+            append_valid_mask=append_valid_mask,
+            crop=crop,
+            crop_size=crop_size,
+            crop_type=crop_type,
+            augment=augment,
+            aug_params=aug_params,
+            sparse_transform=False,
+            norm_params=norm_params,
+        )
+
+        assert (
+            split.lower() == "training" or split.lower() == "validation"
+        ), "Incorrect split values. Accepted split values: training, validation"
+
+        self.is_prediction = is_prediction
+        self.append_valid_mask = append_valid_mask
+
+        if augment:
+            self.augmentor = FlowAugmentor(crop_size=crop_size, **aug_params)
+
+        split = split.lower()
+
+        image_root = osp.join(root_dir, split, "images")
+        
+        if use_backward_flow:
+            flow_root = osp.join(root_dir, split, "backward_flow")
+        else:
+            flow_root = osp.join(root_dir, split, "forward_flow")
+
+        for scene in os.listdir(image_root):
+            image_list = sorted(glob(osp.join(image_root, scene, "*.png")))
+            for i in range(len(image_list) - 1):
+                self.image_list += [[image_list[i], image_list[i + 1]]]
+
+            if not self.is_prediction:
+                self.flow_list += sorted(glob(osp.join(flow_root, scene, "*.flo")))
 
 class CustomDataloaderCreator(DataloaderCreator):
 
@@ -229,7 +321,7 @@ class CustomDataloaderCreator(DataloaderCreator):
 
     def add_Kubric(self, root_dir, split="training", augment=False, **kwargs):
         self.dataset_list.append(
-            Kubric(
+            KubricV2(
                 root_dir,
                 split=split,
                 init_seed=self.init_seed,
--- scripts_flownet_c/exp01.sh
+++ scripts_flownet_c/exp01.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=flownet_c_exp01
 #SBATCH --partition=jiang
 #SBATCH --mem=24G
-#SBATCH --gres=gpu:a5000:1
+#SBATCH --gres=gpu:a6000:1
 #SBATCH --cpus-per-task=8
 #SBATCH --output=../../results/flownet_c/outs/exp01.out
 
@@ -19,8 +19,8 @@ python train.py --model "FlowNetC" \
                 --device "0" \
                 --log_dir "../results/flownet_c/logs/exp01" \
                 --ckpt_dir "../results/flownet_c/ckpts/exp01" \
-                --batch_size 10 \
+                --batch_size 8 \
                 --start_iteration 1 \
-                --num_steps 100100 \
+                --num_steps 1200100 \
                 --train_crop_size 384 448 \
                 --val_crop_size 384 448 
