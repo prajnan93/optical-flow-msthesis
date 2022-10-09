b86c53b8ce61a2cff46bfb0a21dfd2ba71afa2ea
prajnan93 <prajnan.videoworks@gmail.com>
2022-10-09 16:24:37 -0400

    add huggingface swinv2

--- nnflow/models/gmflow/gmflow_v2.py
+++ nnflow/models/gmflow/gmflow_v2.py
@@ -8,6 +8,7 @@ from .matching import global_correlation_softmax, local_correlation_softmax
 from .geometry import flow_warp
 from .utils import normalize_img, feature_add_position
 from .swin_v2 import SwinEncoderV2
+from .swin_v3 import HuggingFaceSwinEncoderV2
 
 from ezflow.models import MODEL_REGISTRY
 from ezflow.modules import BaseModule
