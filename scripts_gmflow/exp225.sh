e9646bf012fa6fef68a416d9df187ff11dd4f902
prajnan93 <prajnan.videoworks@gmail.com>
2022-11-28 15:56:20 -0500

    gmflow experiments

--- scripts_gmflow/exp225.sh
+++ scripts_gmflow/exp225.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 #SBATCH --time=192:00:00
-#SBATCH --job-name=gmflow_exp223
+#SBATCH --job-name=gmflow_exp225
 #SBATCH --partition=jiang
 #SBATCH --mem=24G
 #SBATCH --gres=gpu:a5000:2
 #SBATCH --cpus-per-task=8
-#SBATCH --output=../../results/gmflow/outs/exp223.out
+#SBATCH --output=../../results/gmflow/outs/exp225.out
 
 # GMFlowV2 training with RAFT  Training settings(difference lies in loss fn gamma and scheduler anneal strategy) and Autoflow Augmentations
 # Neighborhood Attention Encoder
 # Effective batch size = 10
+# Disable Eraser augmentation
 
 module load cuda/11.3
 cd ../
 python train.py --model "GMFlowV2" \
                 --model_cfg "./configs/gmflow/models/gmflow_v13.yaml" \
-                --train_cfg "./configs/gmflow/trainer/kubrics_v2_0.yaml" \
+                --train_cfg "./configs/gmflow/trainer/kubrics_v2_2.yaml" \
                 --device "all" \
-                --log_dir "../results/gmflow/logs/exp223" \
-                --ckpt_dir "../results/gmflow/ckpts/exp223" \
+                --log_dir "../results/gmflow/logs/exp225" \
+                --ckpt_dir "../results/gmflow/ckpts/exp225" \
                 --batch_size 5 \
                 --start_iteration 1 \
                 --num_steps 1200100 \
