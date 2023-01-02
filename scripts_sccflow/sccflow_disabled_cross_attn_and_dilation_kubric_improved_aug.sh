23cc94684c413a42d0ac68c5670dd187efd778d0
Prajnan Goswami <prajnan.videoworks@gmail.com>
2023-01-01 23:01:27 -0500

    renamed sscflow scripts and configs

---
+++ scripts_sccflow/sccflow_disabled_cross_attn_and_dilation_kubric_improved_aug.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --time=192:00:00
+#SBATCH --job-name=sccflow_disabled_cross_attn_and_dilation_kubric_improved_aug
+#SBATCH --partition=jiang
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=../../results/sccflow/outs/sccflow_disabled_cross_attn_and_dilation_kubric_improved_aug.out
+
+# SCCflow without Cross Attention and Dilation
+# Effective batch size = 10
+# Disable out of boundary cropping
+
+module load cuda/11.3
+cd ../
+python train.py --model "SCCFlow" \
+                --model_cfg "./configs/sccflow/models/sccflow_disabled_cross_attn_and_dilation.yaml" \
+                --train_cfg "./configs/sccflow/trainer/kubric.yaml" \
+                --device "0" \
+                --log_dir "../results/sccflow/logs/sccflow_disabled_cross_attn_and_dilation_kubric_improved_aug" \
+                --ckpt_dir "../results/sccflow/ckpts/sccflow_disabled_cross_attn_and_dilation_kubric_improved_aug" \
+                --batch_size 10 \
+                --start_iteration 1 \
+                --num_steps 1200100 \
+                --train_crop_size 368 496 \
+                --val_crop_size 368 496
